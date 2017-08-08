20349c0ab093db187fabc449ee9df75a6d2e0830
Srijayanth Sridhar <srijayanth@gmail.com>
2017-08-08 16:23:02 +0530

    Adding some docs to the functions

--- src/tic_tac_toe_bot/utils.clj
+++ src/tic_tac_toe_bot/utils.clj
@@ -1,6 +1,7 @@
 (ns tic-tac-toe-bot.utils)
 
 (def winning-combinations
+  "A set of all winning combinations"
   #{#{1 2 3}
     #{4 5 6}
     #{7 8 9}
@@ -10,9 +11,13 @@
     #{1 5 9}
     #{3 5 7}})
 
-(def is-superset? every?)
+(def is-superset?
+  "Aliasing every? to read differently"
+  every?)
 
-(defn has-won? [player-moves]
+(defn has-won?
+  "This function determines if a set of moves constitutes a winning combination"
+  [player-moves]
   (let [player-moves-is-a-superset-of-at-least-some?
         (partial some (partial is-superset? player-moves))]
     (player-moves-is-a-superset-of-at-least-some? winning-combinations)))
