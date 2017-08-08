6f16e08c72a12be542d73783eb8f2e76fe1339d5
Srijayanth Sridhar <srijayanth@gmail.com>
2017-08-08 15:16:41 +0530

    Tests for rows, cols, diagonals and some failing conditions

--- test/tic_tac_toe_bot/utils_test.clj
+++ test/tic_tac_toe_bot/utils_test.clj
@@ -22,4 +22,26 @@
     (are [player-moves] (true? (has-won? player-moves))
       #{1 2 3}
       #{4 5 6}
-      #{7 8 9})))
+      #{7 8 9}))
+  (testing "Simple passing col conditions"
+    (are [player-moves] (true? (has-won? player-moves))
+      #{1 4 7}
+      #{2 5 8}
+      #{3 6 9}))
+  (testing "Simple passing diagonal conditions"
+    (are [player-moves] (true? (has-won? player-moves))
+      #{1 5 9}
+      #{3 5 7}))
+  (testing "Row conditions when there are other moves"
+    (are [player-moves] (true? (has-won? player-moves))
+      #{1 2 3 4}
+      #{4 5 6 7}
+      #{7 8 9 1}))
+  (testing "Failing conditions"
+    (are [player-moves] (nil? (has-won? player-moves))
+      #{1 2}
+      #{4 5}
+      #{7 8}
+      #{1 4}
+      #{2 5}
+      #{3 6})))
